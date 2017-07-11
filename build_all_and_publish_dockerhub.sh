30f363653e0e71b32dd34b8b3e95b29308eaf597
daverick <daverick@users.noreply.github.com>
2017-07-11 15:55:50 +0200

    change dockerhub repo to daverick

--- build_all_and_publish_dockerhub.sh
+++ build_all_and_publish_dockerhub.sh
@@ -2,21 +2,21 @@
 
 cd shopfront
 mvn clean install
-if docker build -t danielbryantuk/djshopfront . ; then
-  docker push danielbryantuk/djshopfront
+if docker build -t daverick/djshopfront . ; then
+  docker push daverick/djshopfront
 fi
 cd ..
 
 cd productcatalogue
 mvn clean install
-if docker build -t danielbryantuk/djproductcatalogue . ; then
-  docker push danielbryantuk/djproductcatalogue
+if docker build -t daverick/djproductcatalogue . ; then
+  docker push daverick/djproductcatalogue
 fi
 cd ..
 
 cd stockmanager
 mvn clean install
-if docker build -t danielbryantuk/djstockmanager . ; then
-  docker push danielbryantuk/djstockmanager
+if docker build -t daverick/djstockmanager . ; then
+  docker push daverick/djstockmanager
 fi
 cd ..
